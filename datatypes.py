103b658a313ea03fbcc0ff3a30501dd25eb4a13b
Zohaib-Younis <160317382+Zohaib-Younis@users.noreply.github.com>
2026-05-13 08:57:51 +0500

    Type casting is done

--- datatypes.py
+++ datatypes.py
@@ -5,3 +5,8 @@ print(name);
 print(type(name))
 print(len(name))
 
+name2="Shahzaib Younis"
+length=len(name2)
+print(length)
+#here we are doing type casting converting length from int to string so that we can concatenate it with name and name2
+print(name+" "+ str(length)+" "+name2)
