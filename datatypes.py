4f224dc6f7890164381e50c439a87329434fe085
Zohaib-Younis <160317382+Zohaib-Younis@users.noreply.github.com>
2026-05-13 14:02:51 +0500

    I have learn input function type casting , len function

--- datatypes.py
+++ datatypes.py
@@ -10,3 +10,20 @@ length=len(name2)
 print(length)
 #here we are doing type casting converting length from int to string so that we can concatenate it with name and name2
 print(name+" "+ str(length)+" "+name2)
+
+#function to convert type of variable
+'''int()
+float()
+str()
+bool()'''
+
+
+print("10"+ "20") #this will concatenate the two strings and give us 1020
+print(int("10")+ int("20")) #this will convert the strings to integers and then 
+
+
+#input function
+num1=input("Enter first number: ")
+num2=input("Enter second number: ")
+sum=int(num1)+int(num2)
+print("The sum of "+ num1 + " and " + num2 + " is: " + str(sum))
